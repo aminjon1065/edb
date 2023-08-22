98249013530e7628e50f9f89de36d0a5c6e5399b
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-08-22 22:37:15 +0500

    0.0.1

--- app/Http/Controllers/OtherFilesController.php
+++ app/Http/Controllers/OtherFilesController.php
@@ -8,26 +8,34 @@ class OtherFilesController extends Controller
 {
     public function store()
     {
-        $file = request()->file('filename');
-        $filename = $file->getClientOriginalName();
-        $user = auth()->user();
+        try {
+            $file = request()->file('filename');
+            $filename = $file->getClientOriginalName();
+            $user = auth()->user();
 
-        // Сохранение файла
-        $folderName = $user->first_name . '_' . $user->last_name;
-        $path = $file->storeAs("other_files/{$folderName}", $filename, 'public');
+            // Сохранение файла
+            $folderName = $user->first_name . '_' . $user->last_name;
+            $path = $file->storeAs("other_files/{$folderName}", $filename, 'public');
 
-        // Сохранение информации о файле в базе данных
-        $user->otherFiles()->create([
-            'filename' => $filename,
-            'path' => $path
-        ]);
+            // Сохранение информации о файле в базе данных
+            $user->otherFiles()->create([
+                'filename' => $filename,
+                'path' => $path
+            ]);
 
-        // Генерация полного URL файла
-        $fileUrl = asset("storage/other_files/{$folderName}/$filename");  // Обновленный URL файла
+            // Генерация полного URL файла
+            $fileUrl = asset("storage/other_files/{$folderName}/$filename");  // Обновленный URL файла
 
-        return response()->json([
-            'message' => 'Файл успешно загружен',
-            'url' => $fileUrl  // Возвращаем URL файла
-        ]);
+            return response()->json([
+                'message' => 'Файл успешно загружен',
+                'url' => $fileUrl  // Возвращаем URL файла
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Ошибка при загрузке файла',
+                'error' => $th->getMessage()
+            ], 500);
+        }
+
     }
 }
