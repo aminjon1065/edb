a876356883ac73b61ea6ae5d373581bd15f2233a
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-08-24 17:18:48 +0500

    0.0.1

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,9 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-                'host' => env('PUSHER_HOST'),
+                'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'https'
+                'scheme' => 'http',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
