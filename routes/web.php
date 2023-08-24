991e973035f4ffcd15822557ae6a46a2ff63601c
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-08-24 11:31:06 +0500

    0.0.1

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test-notification', function () {
+    event(new \App\Events\NotificationSharedMail('Test Message', 1));
+    return 'Notification sent';
+});
+
 Route::post("/pdf-reports/{lang}", [GetDocumentsController::class, 'pdfReports']);
 
