d2a0618b2a137ff308f3aa20538983a0540f9bde
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-08-24 22:21:25 +0500

    0.0.1

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => env('APP_URL'),
+                'host' => env('PUSHER_HOST'),
                 'port' => 6001,
                 'scheme' => 'http'
             ],
